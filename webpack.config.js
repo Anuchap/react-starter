a0aeedb3edecd51f0ce4e5bd557d1491cc9ec531
anucha8p <anucha8p@gmail.com>
2017-03-10 21:48:48 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    entry: { app: './src/app.js' },
+    output: {
+        filename: './dist/[name].js',
+        publicPath: ''
+    },
+    module: {
+        loaders: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+          { test: /\.json$/, loader: 'json-loader' }
+        ]
+    }
+}
